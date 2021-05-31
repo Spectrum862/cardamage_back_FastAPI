11390bbd16f5169d946bc4a17c39906e6edc4e5d
spectrum862 <supakit.sjd@gmail.com>
2021-06-01 02:01:10 +0700

    [update] base64 response

--- app/routers/machine_learning.py
+++ app/routers/machine_learning.py
@@ -1,39 +1,14 @@
 from fastapi import APIRouter, WebSocket
-from mrcnn.config import Config
 import mrcnn.model as modellib
+from ..utils.ml_helper import get_base64_instant, InferenceConfig
 import os
 import skimage
 import numpy as np
-import io
 
 router = APIRouter(
     prefix='/model',
     tags=['Model']
 )
-
-
-class DamageConfig(Config):
-    NAME = 'damage'
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-    NUM_CLASSES = 4 + 1
-    IMAGE_RESIZE_MODE = 'square'
-    IMAGE_MIN_DIM = 1024
-    IMAGE_MAX_DIM = 1024
-    STEPS_PER_EPOCH = 100
-    VALIDATION_STEPS = 50
-    LEARNING_RATE = 0.001
-    LEARNING_MOMENTUM = 0.9
-    DETECTION_MIN_CONFIDENCE = 0.8
-    GRADIENT_CLIP_NORM = 10.0
-
-
-class InferenceConfig(DamageConfig):
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-    IMAGE_MIN_DIM = 256
-    IMAGE_MAX_DIM = 1024
-    DETECTION_MIN_CONFIDENCE = 0.8
 
 
 @router.post('/predict')
@@ -44,10 +19,12 @@ async def predict():
     model = modellib.MaskRCNN(mode='inference', config=inference_config, model_dir=model_path)
     print('Loading weights from ', model_path)
     model.load_weights(model_path, by_name=True)
-    data_test_path = 'app/asset/dataset/test'
+    data_test_path = 'app/asset/dataset/test_uni'
     image_paths = []
     results = []
+    instant_images = []
     classes = ['BG', '(A)scratch', '(B)minimal pound', '(C)heavy damage', '(D)crush or break']
+
     for filename in os.listdir(data_test_path):
         if os.path.splitext(filename)[1].lower() in ['.png', '.jpg', '.jpeg']:
             image_paths.append(os.path.join(data_test_path, filename))
@@ -56,9 +33,12 @@ async def predict():
         img = skimage.io.imread(image_path)
         img_arr = np.array(img)
         result = model.detect([img_arr], verbose = 1)
+        r = result[0]
         results.append(result[0])
-    print(results[0]['masks'])
-    return {'result': 'eiei'}
+        base64_img = get_base64_instant(img, r['rois'], r['masks'], r['class_ids'], classes, r['scores'], figsize = (12,12))
+        instant_images.append(base64_img)
+    # print(results[0]['masks'])
+    return {'result': instant_images}
 
 
 
