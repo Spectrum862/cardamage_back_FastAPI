de36c71e16c8bf498ff8397a46ae20fbec864dcf
spectrum862 <supakit.sjd@gmail.com>
2021-06-04 05:30:19 +0700

    [update] add base64 image request body

--- app/routers/machine_learning.py
+++ app/routers/machine_learning.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter
 import mrcnn.model as modellib
 from ..utils.ml_helper import get_base64_instant, InferenceConfig
 import os
 import skimage
 import numpy as np
+from pydantic import BaseModel
+from typing import List
+import base64
 
 router = APIRouter(
     prefix='/model',
@@ -11,8 +14,12 @@ router = APIRouter(
 )
 
 
+class PredictRequestBody(BaseModel):
+    images: List[str]
+
+
 @router.post('/predict')
-async def predict():
+async def predict(body: PredictRequestBody):
     inference_config = InferenceConfig()
     inference_config.display()
     model_path = 'app/asset/model.h5'
@@ -29,15 +36,17 @@ async def predict():
         if os.path.splitext(filename)[1].lower() in ['.png', '.jpg', '.jpeg']:
             image_paths.append(os.path.join(data_test_path, filename))
 
-    for image_path in image_paths:
-        img = skimage.io.imread(image_path)
+    for base64image in body.images:
+        if isinstance(base64image, bytes):
+            base64image = base64image.decode("utf-8")
+        imgdata = base64.b64decode(base64image)
+        img = skimage.io.imread(imgdata, plugin='imageio')
         img_arr = np.array(img)
         result = model.detect([img_arr], verbose = 1)
         r = result[0]
         results.append(result[0])
         base64_img = get_base64_instant(img, r['rois'], r['masks'], r['class_ids'], classes, r['scores'], figsize = (12,12))
         instant_images.append(base64_img)
-    # print(results[0]['masks'])
     return {'result': instant_images}
 
 
